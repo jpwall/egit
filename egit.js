0d9e26be7c5d3e84e45715862f49b4f6d5c3552c
jpwall <jessephilipwalling@gmail.com>
2016-02-21 17:05:49 -0800

    make program easier to use by adding explanations of errors

--- egit.js
+++ egit.js
@@ -5,23 +5,27 @@ var addFile = process.argv[2];
 var commitMessage = process.argv[3];
 var branch = process.argv[4];
 
-require('simple-git')()
-    .add(addFile, function (err, data) {
-	if(err) {
-	    console.log('Please enter a valid file name');
-	}
-    })
+if (addFile == undefined || commitMessage == undefined || branch == undefined) {
+    console.log('Please enter valid parameters to use egit (easy-git) \n');
+    console.log('To push example.txt to branch master with commit message "test" execute:');
+    console.log('egit example.txt "test" master \n');
+}
+else {
+    require('simple-git')()
+	.add(addFile, function (err, data) {
+	    if(err) {
+		console.log('Please enter a valid file name');
+	    }
+	})
 
-    .commit(commitMessage, function (err, data) {
-	if(err) {
-	    console.log(err);
-	}
-    })
+	.commit(commitMessage, function (err, data) {
+	})
 
-    .push('origin', branch, function (err, data) {
-	/*if(err) {
-	    console.log(branch + ' does not seem to be a valid branch');
-	}*/
-	//console.log(data);
-    });
+	.push('origin', branch, function (err, data) {
+	    if(err) {
+		console.log(branch + ' does not seem to be a valid branch');
+	    }
+	});
+
+}
 
